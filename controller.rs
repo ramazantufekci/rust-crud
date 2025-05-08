978f0f71dd63fec1f59c0349d4af262e39f00cb0
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2025-05-08 16:32:25 +0300

    Update controller.rs

--- controller.rs
+++ controller.rs
@@ -3,6 +3,8 @@ use axum::http::StatusCode;
 use axum::Json;
 use serde_json::Value;
 
+use crate::model{User, UserInfo};
+
 pub async fn list_users() ->(StatusCode, Json<Value>) {
   
 }
@@ -11,11 +13,11 @@ pub async fn get_user_by_id(Path(id):Path<u64>) -> (StatusCode,Json<Value>) {
   
 }
 
-pub async fn create_user(Json(user):Json<User>) -> StatusCode {
+pub async fn create_user(Json(user):Json<UserInfo>) -> StatusCode {
   
 }
 
-pub async fn update_user(Path(id):Path<u64>, Json(user):Json<User>) -> StatusCode {
+pub async fn update_user(Path(id):Path<u64>, Json(user):Json<UserInfo>) -> StatusCode {
   
 }
 
